e650ff2718e707dde5c962f5a342c4fe5e932357
denis daros <denisv.daros@gmail.com>
2023-03-13 17:28:24 -0300

    criação de classe dominio

---
+++ src/Domains/Car.ts
@@ -0,0 +1,82 @@
+import ICar from '../Interfaces/ICar';
+
+class Car {
+  private id?: string | undefined;
+  private model: string;
+  private year: number;
+  private color: string;
+  private status?: boolean | undefined;
+  private buyValue: number;
+  private doorsQty: number;
+  private seatsQty: number;
+  
+  // constructor não permite mais de 8 parametros por isso a criação de um paramentro de objeto com o tipo de interface Icar
+  constructor(car: ICar) {
+    this.id = car.id;
+    this.model = car.model;
+    this.year = car.year;
+    this.color = car.color;
+    this.status = car.status;
+    this.doorsQty = car.doorsQty;
+    this.buyValue = car.buyValue;
+    this.seatsQty = car.seatsQty;
+  }
+
+  public getId(): string | undefined {
+    return this.id;
+  }
+  public setId(value: string | undefined) {
+    this.id = value;
+  }
+
+  public getModel(): string {
+    return this.model;
+  }
+  public setModel(value: string) {
+    this.model = value;
+  }
+
+  public getYear(): number {
+    return this.year;
+  }
+  public setYear(value: number) {
+    this.year = value;
+  }
+
+  public getColor(): string {
+    return this.color;
+  }
+  public setColor(value: string) {
+    this.color = value;
+  }
+
+  public getStatus(): boolean | undefined {
+    return this.status;
+  }
+  public setStatus(value: boolean | undefined) {
+    this.status = value;
+  }
+
+  public getBuyValue(): number {
+    return this.buyValue;
+  }
+  public setBuyValue(value: number) {
+    this.buyValue = value;
+  }
+
+  public getDoorsQty(): number {
+    return this.doorsQty;
+  }
+  public setDoorsQty(value: number) {
+    this.doorsQty = value;
+  }
+
+  public getSeatsQty(): number {
+    return this.seatsQty;
+  }
+  public setSeatsQty(value: number) {
+    this.seatsQty = value;
+  }
+}
+
+export default Car;
