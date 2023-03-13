c0a025e2b3fd1aff1c7f085e652dab2d869d7bd6
denis daros <denisv.daros@gmail.com>
2023-03-13 19:12:18 -0300

    criando a rota /cars

---
+++ src/Routes/Routes.ts
@@ -0,0 +1,13 @@
+// src/Routes/Routes.ts
+
+import { Router } from 'express';
+import CarController from '../Controllers/CarController';
+
+const routes = Router();
+
+routes.post(
+  '/cars',
+  (req, res, next) => new CarController(req, res, next).insert(),
+);
+
+export default routes;
